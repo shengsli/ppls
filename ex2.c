94bffc8d94ee07fee7a56bfcf199be18034055c3
shengsli <sheng.s.li@outlook.com>
2019-03-09 17:12:55 +0000

    update

--- ex2.c
+++ ex2.c
@@ -12,13 +12,16 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+pthread_barrier_t barrier;
+pthread_barrier_t barrier2;
+
 // Print a helpful message followed by the contents of an array
 // Controlled by the value of SHOWDATA, which should be defined
 // at compile time. Useful for debugging.
 void showdata (char *message,  int *data,  int n) {
   int i; 
 
-if (SHOWDATA) {
+  if (SHOWDATA) {
     printf (message);
     for (i=0; i<n; i++ ){
      printf (" %d", data[i]);
@@ -48,27 +51,105 @@ void sequentialprefixsum (int *data, int n) {
 }
 
 // MY THREAD CODE
-void *thread_sequentialprefixsum (void *args) {
+typedef struct arg_pack {
+  int tid;
   int *data;
   int n;
+} arg_pack;
 
+typedef arg_pack *argptr;
+
+void *thread_sequentialprefixsum (void *args) {
+  int tid;
+  int *data;
+  int n; // allocated chunk size, varies among threads
   // retrieve the arguments
-  a = ((arg_pack *)args)->myarg1;
-  b = ((arg_pack *)args)->myarg2;
-  c = ((arg_pack *)args)->myarg3;
+  tid = ((arg_pack *)args)->tid;
+  data = ((arg_pack *)args)->data;
+  n = ((arg_pack *)args)->n;
 
-  // use them
-  printf ("Hello from thread %d, my other arguments were %d and %d.\n", a, b, c);
+  printf ("Hello from thread %d, i have %d items.\n", tid, n);
+  sequentialprefixsum(data, n);
+  pthread_barrier_wait(&barrier);
+  pthread_barrier_wait(&barrier2);
+  printf("this msg should show up after phase 2.\n");
+  int chunk_size = n / NTHREADS;
+  // phase 3: local updates
+  if (tid == 0) return NULL;
+  int i;
+  //printf("%d", tid); showdata("***thread debug", data, n);
+  for (i=0; i<n-1; ++i)
+  {
+	printf("%d -> ",*(data+i));
+  	*(data+i) += *(data-1);
+	printf("%d\n",*(data+i));
+  }
+  showdata("***thread debug", data, n);
+  return NULL;
+}
+
+// thread 0 perform sequential prefix sum at highest indices of each chunk 
+void *parent_sequentialprefixsum (int *data, int n) {
+  // perform NTHREADS-1 times sum, from 2nd chunk to the last one
+  int chunk_size = n / NTHREADS; // minimum # of items
+  int i;
+  for (i=1; i<NTHREADS-1; i++) { // 2nd chunk to 2nd last chunk
+	/* printf("data[%d] = %d + %d\n", */
+	/* 	   (i+1)*chunk_size-1, data[(i+1)*chunk_size-1], data[i*chunk_size-1]); */
+    data[(i+1)*chunk_size-1] = data[(i+1)*chunk_size-1] + data[i*chunk_size-1];
+	//showdata("***debug", data, NITEMS);
+  }
+  /* printf("data[%d] = %d + %d\n", */
+  /* 		 NITEMS-1, data[NITEMS-1], data[(NTHREADS-1)*chunk_size-1]); */
+  data[NITEMS-1] += data[(NTHREADS-1)*chunk_size-1];
+  //showdata("***debug", data, NITEMS);
   return NULL;
 }
 
 // YOU MUST WRITE THIS FUNCTION AND ANY ADDITIONAL FUNCTIONS YOU NEED
 void parallelprefixsum (int *data, int n) {
-    printf("call parallel prefix sum");
-	pthread_t thread[P];
-	int i;
-	for (i=0; i<NTHREADS, I++)
-	  pthread_create(&thread[i], NULL, sequentialprefixsum, NULL);
+  printf("call parallel prefix sum\n");
+  pthread_t *threads;
+  arg_pack *threadargs;
+  int i;
+  threads = (pthread_t *) malloc (NTHREADS * sizeof(pthread_t));
+  threadargs = (arg_pack *) malloc (NTHREADS * sizeof(arg_pack));
+  if (pthread_barrier_init(&barrier, NULL, NTHREADS+1)) { // create a barrier
+	printf("fail to init barrier\n");
+	return;
+  }
+  // phase 2: thread 0 performs sequential prefix sum to the last item of each chunk
+  if (pthread_barrier_init(&barrier2, NULL, NTHREADS+1)) { // create a barrier
+	printf("fail to init barrier2\n");
+	return;
+  }
+  
+  // phase 1:
+  int chunk_size = n / NTHREADS;
+  for (i=0; i<NTHREADS; i++)   // allocate items to threads
+  {
+	threadargs[i].tid = i;
+	threadargs[i].data = data + (chunk_size * i);
+	threadargs[i].n = chunk_size;
+  }
+  threadargs[NTHREADS-1].n += n - chunk_size * NTHREADS; // give rest items to the last thread
+  for (i=0; i<NTHREADS; i++) // create threads
+	pthread_create(&threads[i], NULL, thread_sequentialprefixsum, (void *) &threadargs[i]);
+  pthread_barrier_wait(&barrier); // snyc
+  printf("synchronization is done.\n");
+  pthread_barrier_destroy(&barrier);
+  
+  parent_sequentialprefixsum(data, n);
+  showdata("***debug: phase 2 done: ", data, NITEMS);
+  pthread_barrier_wait(&barrier2);
+  pthread_barrier_destroy(&barrier2);
+  
+  // phase 3: child threads do local update
+
+  // join and exit
+  for (i=0; i<NTHREADS; i++)
+  	pthread_join(threads[i], NULL);
+  showdata("***debug", data, NITEMS);
 }
 
 
---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+test_num=10
+succ_num=0
+for i in `seq 1 $test_num`; do
+    gcc -o ex2 ex2.c -DNITEMS=10 -DNTHREADS=4 -DSHOWDATA=1 -lpthread
+    count=`./ex2 | grep -c "Well done"`
+	((succ_num++))
+	if [[ $count = 0 ]]; then
+		echo "test $count failed."
+		exit -1;
+	else
+		echo "test $i succeeded."
+	fi
+done
+
+if [ "$succ_num" == "$test_num" ]; then
+	echo "well done. all $test_num tests succeeded."
+fi
